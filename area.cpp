2f460a078a8e6152284aebc8feb8aa514c5ad726
Ashiqur Rahman <ashiqurrahman3529@gmail.com>
2025-05-28 20:00:01 +0600

    Area

---
+++ area.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    double area,hight,base;
+
+
+    cout<<"Enter base = ";
+    cin>>base;
+
+    cout<<"Enter hight = ";
+    cin>>hight;
+
+
+ area= 0.5*base*hight;
+ cout<<"Area of tringle = "<<area;
+
+
+}
